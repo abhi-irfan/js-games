a07af117ca49f9784a6be6cadfbb5efe75261b3b
Dhanu K <bewithdhanu@gmail.com>
2023-05-08 13:36:08 +0530

    send data as json string

--- script.js
+++ script.js
@@ -90,14 +90,14 @@ function checkMatch() {
             if (/Android/.test(navigator.userAgent)) {
                 // The WebView is on an Android device
                 if (typeof MyApp !== "undefined") {
-                    window.MyApp[match ? "onSuccess" : "onFailure"](message, JSON.stringify({}));
+                    window.MyApp[match ? "onSuccess" : "onFailure"](message, "");
                 } else {
                     console.error("MyApp object is not defined.");
                 }
             } else if (/iPhone|iPad|iPod|iOS/.test(navigator.userAgent)) {
                 // The WebView is on an iOS device
                 if (typeof webkit !== "undefined") {
-                    window.webkit.messageHandlers.MyApp.postMessage({ event: match ? "onSuccess" : "onFailure", data: JSON.stringify({}) });
+                    window.webkit.messageHandlers.MyApp.postMessage({ event: match ? "onSuccess" : "onFailure", data: "");
                 } else {
                     console.error("webkit object is not defined.");
                 }
